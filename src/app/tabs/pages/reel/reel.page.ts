51ac33e0b49a986cb51ddac75941cd6bb4c8bd6d
hari-hk <hariharakumar5196@gmail.com>
2021-01-28 19:43:06 +0530

    added reel page

--- src/app/tabs/pages/reel/reel.page.ts
+++ src/app/tabs/pages/reel/reel.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-reel',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReelPage implements OnInit {
 
-  constructor() { }
+  list = [];
+  option = {
+    loop: true,
+    direction: 'vertical'
+  };
+  constructor(public api: ApiService) { }
 
   ngOnInit() {
+    this.getSearchList();
+  }
+  getSearchList() {
+    this.api.getImageList({ page: 20, limit: 20 }).subscribe(result => {
+      this.list = result;
+    });
   }
 
 }
